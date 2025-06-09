e56909b7f59d84daec1ae6b4252082e92a66d22a
Maye199819 <suxomayerli@gmail.com>
2025-06-08 23:40:35 -0400

    Create Cliente.java

---
+++ PERSISTENCIA/EJERCICIO3/JAVA/Cliente.java
@@ -0,0 +1,26 @@
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
+    private int id;
+    private String nombre;
+    private int telefono;
+
+    public Cliente(int id, String nombre, int telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [id=" + id + ", nombre=" + nombre + ", telefono=" + telefono + "]";
+    }
+}
